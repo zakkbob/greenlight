835ca0295c07d3a242140a84a8e831d6d71a2c3a
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-09-03 01:04:08 +0100

    Chapter 9.5. Full-text search

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -157,7 +157,7 @@ func (m *MovieModel) Delete(id int64) error {
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
 	query := `SELECT id, created_at, title, year, runtime, genres, version
 		FROM movies
-		WHERE (LOWER(title) = LOWER($1) OR $1 = '')
+		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '')
 		AND (genres @> $2 OR $2 = '{}')
 		ORDER BY id`
 
