a8c15030bfc9c5454e07ee1e63a7d49666bcc9f7
zakkbob <114868973+zakkbob@users.noreply.github.com>
2025-09-05 23:14:45 +0100

    Chapter 15.3. Authenticating requests

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+func (app *application) invalidAuthenticationTokenResponse(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("WWW-Authenticate", "Bearer")
+
+	message := "invalid or missing authentication token"
+	app.errorResponse(w, r, http.StatusUnauthorized, message)
+}
+
 func (app *application) invalidCredentialsResponse(w http.ResponseWriter, r *http.Request) {
 	message := "invalid authentication credentials"
 	app.errorResponse(w, r, http.StatusUnauthorized, message)
